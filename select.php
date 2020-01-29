13262bb0d98443d27e6cc21044f05e5ab55b33bb
Shinji-MCZ <yoshinoya1234@gmail.com>
2020-01-30 01:23:24 +0900

    1st commit

--- select.php
+++ select.php
@@ -1,3 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>データベース課題2</title>
+</head>
+<body>
+  
+  <h1>本日のご紹介ペット！</h1>
+    <p>
+      <form action="" method="">キーワード:
+      <input type="text" placeholder="キーワードを入力">
+      <input type="submit" value="送信">
+    </p>
+</body>
+</html>
+
 <?php
 
 define('DSN', 'mysql:host=mysql;dbname=pet_shop;charset=utf8;');
@@ -6,7 +25,6 @@ define('PASSWORD', '9999');
 
 try {
   $dbh = new PDO(DSN, USER, PASSWORD);
-  echo '接続に成功しました！' . '<br><br>';
 } catch (PDOException $e) {
   echo $e->getMessage(),
   exit;
@@ -17,7 +35,13 @@ $stmt = $dbh->prepare($sql);
 $stmt->execute();
 $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$sql = "SELECT * from animals WHERE description LIKE '%おっとり%'";
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+$animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 foreach ($animals as $animal)
 {
   echo $animal['type'] .  ' の ' . $animal['classifcation'] . ' ちゃん ' .' <br> ' . $animal['description'] . ' <br> ' . $animal['birthday'] . ' 生まれ ' . ' <br> ' . ' 出身地 ' . $animal['birthplace'] . ' <hr> ';
-}  
+}
+?>
